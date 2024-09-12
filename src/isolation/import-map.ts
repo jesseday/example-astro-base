eec290ecd1b578c8af93a8ae283e4b4a555ff677
Jesse Day <jesse.day@adaptagency.com>
2024-09-12 18:08:06 -0400

    Just playing around

--- src/isolation/import-map.ts
+++ src/isolation/import-map.ts
@@ -1,3 +1,5 @@
+import Hero from "@components/ui/Hero.astro";
 import Body from "@components/ui/Body.astro";
 import Section from "@components/ui/Section.astro";
-export default [Body, Section];
+import Duplex from "@components/ui/Duplex.astro";
+export default [Body, Section, Hero, Duplex];
