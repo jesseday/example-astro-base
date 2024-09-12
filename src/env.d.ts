d43316b7a4cf85a920c4fbe2f209c165bfa98fda
Jesse Day <jesse.day@adaptagency.com>
2024-09-12 17:52:53 -0400

    Add types for environment vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 import "../.astro/types.d.ts";
+
+interface ImportMetaEnv {
+  readonly CONTENTFUL_SPACE: string;
+  readonly CONTENTFUL_ENVIRONMENT: string;
+  readonly CONTENTFUL_ACCESS_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly PREVIEW: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
